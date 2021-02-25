9c1133ca7aab586ac01be5c0262a883df2e7c1cb
Tarek Behairy <tbtarek@gmail.com>
2021-02-24 22:16:18 -0500

    new note updated in server js

--- server.js
+++ server.js
@@ -15,3 +15,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public', '/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public', '/notes.html')));
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = uuidv4();
+  const newN = req.body
+  dataB.push(newN);
+  fs.writeFileSync('./db/db.json', JSON.stringify(DB));
+  res.json(dataB);
